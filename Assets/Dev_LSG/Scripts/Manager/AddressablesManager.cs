fb73ee656bd177b1e4790ca62c2044712feac101
dev-LS-archive <gyu3941@gmail.com>
2022-10-05 17:38:25 +0900

    2022-10-05 PM

---
+++ Assets/Dev_LSG/Scripts/Manager/AddressablesManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+namespace Dev_LSG.Scripts.Manager
+{
+    public class AddressablesManager : MonoBehaviour
+    {
+        [SerializeField]
+        private AssetReference playerXRRigAssetReference;
+
+        [SerializeField] private AssetReference handsMatAssetReference;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
