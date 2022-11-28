ab74b13dd3b40f9e6a6de9ddc1f44710e6811644
dev-LS-archive <gyu3941@gmail.com>
2022-11-28 16:32:40 +0900

    Crest_Ocean

--- Assets/Dev_LSG/Scripts/Event/AnimationActEvent.cs
+++ Assets/Dev_LSG/Scripts/Event/AnimationActEvent.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using UnityEngine.Events;
+
 
 namespace Dev_LSG.Scripts.Event
 {
     public class AnimationActEvent : MonoBehaviour
     {
         public GameObject obj;
+        public UnityEvent animEvent;
 
+        public void AnimEvent()
+        {
+            animEvent.Invoke();
+        }
         public void Act()
         {
             //print("act");
