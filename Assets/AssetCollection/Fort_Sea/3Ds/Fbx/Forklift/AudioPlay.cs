7b03d3dcd0557475836e3e37a1ed3b280e70fef7
dev-LS-archive <gyu3941@gmail.com>
2022-11-29 18:35:35 +0900

    Fort

---
+++ Assets/AssetCollection/Fort_Sea/3Ds/Fbx/Forklift/AudioPlay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPlay : MonoBehaviour 
+{
+    public void playClip()
+    {
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+}
---
+++ Assets/AssetCollection/Fort_Sea/3Ds/Scripts/Forklift.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Forklift : MonoBehaviour
+{
+
+    public AudioSource audioSrc;
+    public AudioClip Forkact;
+    public GameObject Dust_A;
+    public GameObject Dust_B;
+
+
+    void Awake()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    public void Forksnd()
+    {
+        audioSrc.loop = false;
+        audioSrc.clip = Forkact;
+        audioSrc.Play();
+    }
+
+    public void dustA1()
+    {
+        Dust_A.SetActive(true);
+    }
+
+    public void dustA2()
+    {
+        Dust_A.SetActive(false);
+    }
+
+    public void dustB1()
+    {
+        Dust_B.SetActive(true);
+    }
+
+    public void dustB2()
+    {
+        Dust_B.SetActive(false);
+    }
+
+}
