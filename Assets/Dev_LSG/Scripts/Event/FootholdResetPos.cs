dd3ed22502914d56a9d283c05217bca201b1e8e8
dev-LS-archive <gyu3941@gmail.com>
2022-11-30 11:37:35 +0900

    11:37

--- Assets/Dev_LSG/Scripts/Event/FootholdResetPos.cs
+++ Assets/Dev_LSG/Scripts/Event/FootholdResetPos.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Dev_LSG.Scripts.Event
 {
     public class FootholdResetPos : MonoBehaviour
     {
         public Transform resetPoint;
+        public UnityEvent eventReset;
 
         public void ChangeResetPos()
         {
             var pos = transform.position;
             resetPoint.position = new Vector3(pos.x, resetPoint.position.y, pos.z);
+            eventReset.Invoke();
         }
     }
 }
