6f934ab060b938a9ab717629b4bfa93f4bfea091
dev-LS-archive <gyu3941@gmail.com>
2022-12-09 16:38:28 +0900

    2022-12-09_Build

--- Assets/Dev_LSG/Scripts/Event/FootholdResetPos.cs
+++ Assets/Dev_LSG/Scripts/Event/FootholdResetPos.cs
@@ -14,5 +14,11 @@ namespace Dev_LSG.Scripts.Event
             resetPoint.position = new Vector3(pos.x, resetPoint.position.y, pos.z);
             eventReset.Invoke();
         }
+        public void ChangeResetPosAll()
+        {
+            var pos = transform.position;
+            resetPoint.position = new Vector3(pos.x, pos.y, pos.z);
+            eventReset.Invoke();
+        }
     }
 }
