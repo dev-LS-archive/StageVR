814b304b0eaceb2ffa0569e677a7a96528d1b561
dev-LS-archive <gyu3941@gmail.com>
2023-02-16 17:48:46 +0900

    2023-02-16

--- Assets/Dev_LSG/Scripts/Localize/LocalizeAnimatorEvent.cs
+++ Assets/Dev_LSG/Scripts/Localize/LocalizeAnimatorEvent.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Localization;
-using UnityEngine.Localization.Events;
 using UnityEngine.Localization.Components;
-using UnityEngine.UI;
 
-[AddComponentMenu("Localization/Localize Animator Event")]
+[AddComponentMenu("Localization/Asset/Localize Animator Event")]
 public class LocalizeAnimatorEvent : LocalizedAssetEvent<RuntimeAnimatorController, LocalizedAsset<RuntimeAnimatorController>, UnityEvent<RuntimeAnimatorController>>
 {
 }
