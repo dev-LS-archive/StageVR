a18a2d5b62ed79f3c83f2facfdd75c3d945e792f
dev-LS-archive <98000578+dev-LS-archive@users.noreply.github.com>
2023-03-22 18:03:08 +0900

    cam fix

--- Assets/Dev_LSG/Scripts/Event/DelayEvent.cs
+++ Assets/Dev_LSG/Scripts/Event/DelayEvent.cs
@@ -21,7 +21,7 @@ namespace Dev_LSG.Scripts.Event
         }
         public void InvokeEvent()
         {
-            print( gameObject.name+" / invoke");
+            //print( gameObject.name+" / invoke");
             delayEvent.Invoke();
         }
         IEnumerator DelayInvoke()
