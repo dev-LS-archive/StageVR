e0d1fde3f478e9256a3e63d2b182cde71f2b8edc
dev-LS-archive <98000578+dev-LS-archive@users.noreply.github.com>
2023-09-12 11:23:29 +0900

    .

---
+++ Assets/Dev_LSG/Scripts/Event/ActiveEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Dev_LSG.Scripts.Event
+{
+    public class ActiveEvent : MonoBehaviour
+    {
+        public UnityEvent enableEvent;
+
+        private void OnEnable()
+        {
+            enableEvent.Invoke();
+        }
+    }
+}
