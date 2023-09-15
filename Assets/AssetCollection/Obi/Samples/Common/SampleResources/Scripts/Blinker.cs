cfbeae42a002303dcef3529fe3ca9e226922a415
dev-LS-archive <98000578+dev-LS-archive@users.noreply.github.com>
2023-09-15 16:38:38 +0900

    2023-09-15

--- Assets/AssetCollection/Obi/Samples/Common/SampleResources/Scripts/Blinker.cs
+++ Assets/AssetCollection/Obi/Samples/Common/SampleResources/Scripts/Blinker.cs
@@ -2,22 +2,22 @@
 
 public class Blinker : MonoBehaviour {
 
-	public Color highlightColor;
+    public Color highlightColor;
 
- 	private Renderer rend;
-	private Color original;
+     private Renderer rend;
+    private Color original;
 
-	void Awake(){
-		rend = GetComponent<Renderer>();
-		original = rend.material.color;
-	}
+    void Awake(){
+        rend = GetComponent<Renderer>();
+        original = rend.material.color;
+    }
 
-	public void Blink(){
-		rend.material.color = highlightColor;
-	}
+    public void Blink(){
+        rend.material.color = highlightColor;
+    }
 
-	void LateUpdate(){
-		rend.material.color += (original - rend.material.color)*Time.deltaTime*5;
-	}
+    void LateUpdate(){
+        rend.material.color += (original - rend.material.color)*Time.deltaTime*5;
+    }
 
 }
