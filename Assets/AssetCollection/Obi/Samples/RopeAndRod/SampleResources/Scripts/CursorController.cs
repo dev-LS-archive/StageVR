53cc1780d8f10ebde2509d452d11396d3894f4c9
dev-LS-archive <98000578+dev-LS-archive@users.noreply.github.com>
2023-09-20 17:16:42 +0900

    .

--- Assets/AssetCollection/Obi/Samples/RopeAndRod/SampleResources/Scripts/CursorController.cs
+++ Assets/AssetCollection/Obi/Samples/RopeAndRod/SampleResources/Scripts/CursorController.cs
@@ -7,39 +7,39 @@ using Obi;
 public class CursorController : MonoBehaviour
 {
 
-	ObiRopeCursor cursor;
-	ObiRope rope;
-	public float minLength = 0.1f;
+    ObiRopeCursor cursor;
+    ObiRope rope;
+    public float minLength = 0.1f;
     public float speed = 1;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         rope = GetComponent<ObiRope>();
         cursor = GetComponent<ObiRopeCursor>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+    
+    // Update is called once per frame
+    void Update ()
     {
-		if (Input.GetKey(KeyCode.W) && cursor != null)
+        if (Input.GetKey(KeyCode.W) && cursor != null)
         {
             if (rope.restLength > minLength)
                 cursor.ChangeLength(rope.restLength - speed * Time.deltaTime);
-		}
+        }
 
-		if (Input.GetKey(KeyCode.S) && cursor != null)
+        if (Input.GetKey(KeyCode.S) && cursor != null)
         {
             cursor.ChangeLength(rope.restLength + speed * Time.deltaTime);
-		}
+        }
 
-		if (Input.GetKey(KeyCode.A)){
-			rope.transform.Translate(Vector3.left * Time.deltaTime,Space.World);
-		}
+        if (Input.GetKey(KeyCode.A)){
+            rope.transform.Translate(Vector3.left * Time.deltaTime,Space.World);
+        }
 
-		if (Input.GetKey(KeyCode.D)){
-			rope.transform.Translate(Vector3.right * Time.deltaTime,Space.World);
-		}
+        if (Input.GetKey(KeyCode.D)){
+            rope.transform.Translate(Vector3.right * Time.deltaTime,Space.World);
+        }
 
-	}
+    }
 }
