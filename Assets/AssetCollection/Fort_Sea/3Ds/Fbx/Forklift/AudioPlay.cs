fb11047e1fbb8243b0bd546d10b7a68a05b2eb2c
dev-LS-archive <98000578+dev-LS-archive@users.noreply.github.com>
2023-11-23 11:15:18 +0900

    .

--- Assets/AssetCollection/Fort_Sea/3Ds/Fbx/Forklift/AudioPlay.cs
+++ Assets/AssetCollection/Fort_Sea/3Ds/Fbx/Forklift/AudioPlay.cs
@@ -8,15 +8,15 @@ public class AudioPlay : MonoBehaviour
         gameObject.GetComponent<AudioSource>().Play();
     }
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start () 
     {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () 
+        
+    }
+    
+    // Update is called once per frame
+    void Update () 
     {
-	    
-	}
+        
+    }
 }
